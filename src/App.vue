ae4b0e33be8d37521ddc37f64aaea359e4e16b40
Danna Iorio <dannadra@gmail.com>
2026-05-05 20:27:20 -0300

    cambios en app vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,7 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
-  <router-view/>
+  <router-view />
   <router-link to="/">Home</router-link>
   <router-link to="/catalogo">Catálogo</router-link>
   <router-link to="/favoritos">Favoritos</router-link>
